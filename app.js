bf9a1489eea51a5e9bef19ce64f834fb24e3cfca
HUHGEON <rjs2337@naver.com>
2024-08-22 23:38:28 +0900

    게시글 api 수정본

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import { DATABASE_URL } from './env.js';
 import Post from './models/Post.js';
 
-mongoose.connect(DATABASE_URL).then(() => console.log('Connected to MongoDB'));
+mongoose.connect(DATABASE_URL).then(() => console.log('Connected to DB'));
 
 const app = express();
 app.use(express.json());
